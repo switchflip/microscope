bba164c4fbc685461e49dc25daf396f514b79491
Thomas Berry <thom.berry@gmail.com>
2014-04-22 11:46:38 -0400

    Made a simple publication/subscription for comments.

--- server/publications.js
+++ server/publications.js
@@ -6,6 +6,6 @@ Meteor.publish('blogs', function() {
   return Blogs.find();
 })
 
-Meteor.publish('comments', function() {
-return Comments.find();
-})
+Meteor.publish('comments', function(postId) {
+  return Comments.find({postId: postId});
+});
